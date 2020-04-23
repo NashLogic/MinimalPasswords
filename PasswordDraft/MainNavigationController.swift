14011c125785c58789c92af8348cb00b231fd8bf
NashLogic <njkglover@gmail.com>
2020-04-23 21:14:07 +1000

    Pushing in essential files
    All work done until present now pushed into GitHub

---
+++ PasswordDraft/MainNavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  MainNavigationController.swift
+//  PasswordDraft
+//
+//  Created by Nicholas Glover on 19/1/20.
+//  Copyright © 2020 Nicholas Glover. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
