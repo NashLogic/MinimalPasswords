26ca30986537fc99efb89f3f8e54dc06ff99969e
NashLogic <njkglover@gmail.com>
2020-05-10 17:30:49 +1000

    Integrated CocoaPods and renamed files from ViewController to Screen

---
+++ DerivedData/PasswordDraft/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-PasswordDraft.build/Objects-normal/x86_64/Pods-PasswordDraft-dummy 2.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/DerivedData/PasswordDraft/Build/Products/Debug-iphonesimulator/SQLite.swift/SQLite.modulemap \
+  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/Target\ Support\ Files/Pods-PasswordDraft/Pods-PasswordDraft-dummy.m
---
+++ Podfile
@@ -0,0 +1,12 @@
+
+platform :ios, '11.0'
+
+target 'PasswordDraft' do
+
+  # This is for Swift
+  # use_frameworks!
+
+  # List of Pods
+  pod 'SQLite.swift', '~> 0.12.0'
+
+end
