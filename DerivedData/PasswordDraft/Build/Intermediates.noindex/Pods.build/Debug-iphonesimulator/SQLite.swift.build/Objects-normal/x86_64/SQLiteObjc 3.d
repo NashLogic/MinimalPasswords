c9853c7b03f3d217db8b94eef05853bdf4d3b400
NashLogic <njkglover@gmail.com>
2020-05-12 23:32:57 +1000

    pLists and other small files updated

---
+++ DerivedData/PasswordDraft/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SQLite.swift.build/Objects-normal/x86_64/SQLiteObjc 3.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/SQLite.swift/Sources/SQLiteObjc/SQLiteObjc.m \
+  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/Target\ Support\ Files/SQLite.swift/SQLite.swift-prefix.pch \
+  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/SQLite.swift/Sources/SQLiteObjc/include/SQLiteObjc.h \
+  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/SQLite.swift/Sources/SQLiteObjc/fts3_tokenizer.h
