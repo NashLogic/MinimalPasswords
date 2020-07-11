a5a354f05704ebaa136c98df47448b9f55f7eadf
NashLogic <njkglover@gmail.com>
2020-07-11 21:41:46 +1000

    Major folder restructure, gitignore added, deleted extra files

--- DerivedData/MinimalPasswords/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-MinimalPasswords.build/Objects-normal/x86_64/Pods-MinimalPasswords-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/DerivedData/MinimalPasswords/Build/Products/Debug-iphonesimulator/SQLite.swift/SQLite.modulemap \
-  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/Target\ Support\ Files/Pods-MinimalPasswords/Pods-MinimalPasswords-dummy.m \
-  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/DerivedData/PasswordDraft/Build/Products/Debug-iphonesimulator/SQLite.swift/SQLite.modulemap
--- DerivedData/MinimalPasswords/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SQLite.swift.build/Objects-normal/x86_64/SQLite.swift-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/Target\ Support\ Files/SQLite.swift/SQLite.swift-dummy.m \
-  /Users/nicholasglover/Documents/GitHub/MinimalPasswords/Pods/Target\ Support\ Files/SQLite.swift/SQLite.swift-prefix.pch
