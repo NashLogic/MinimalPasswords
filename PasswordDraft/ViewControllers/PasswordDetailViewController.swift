a83a816144b42ead1c56d0a3fccdc88a9502f8ad
NashLogic <njkglover@gmail.com>
2020-08-13 21:09:51 +1000

    Buggy but started PasswordDetailsVC and cleaned up colours in GeneratePasswordVC

---
+++ PasswordDraft/ViewControllers/PasswordDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PasswordDetailViewController.swift
+//  MinimalPasswords
+//
+//  Created by Nicholas Glover on 13/8/20.
+//  Copyright © 2020 Nicholas Glover. All rights reserved.
+//
+
+import Foundation
